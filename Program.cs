ea3abe10b28ffe78b0ae975bc101ebf5db4e2863
non sof <sofinalove2012@gmail.com>
2023-07-02 08:43:35 +0300

    first method

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int lenght = LenghtOfArray();
+
+int LenghtOfArray ()
+{
+Console.WriteLine ("u need to write a lenght of array here");
+int l = Convert.ToInt32(Console.ReadLine());
+return l;
+}
+
+// string[] array = new string;
+
